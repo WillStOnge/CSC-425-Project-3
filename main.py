39af6c6238da9f5adedc68bcdb63a6649394a633
Will St. Onge <ongewill@gmail.com>
2020-11-03 18:47:26 -0500

    Moved some stuff around in main

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import numpy as np, pandas as pd
 from ml_algorithms import DecisionTree
 
 def main():
+    # ----------------------------------------------------------- #
+    #                       Decision Tree                         #
+    # ----------------------------------------------------------- #
+
     # Train the tree.
     train_data = pd.read_csv("datasets/decision_tree/titanic_train.csv").fillna(0)
     tree = DecisionTree(train_data.to_numpy()[:, :-1], train_data.to_numpy()[:, -1])
@@ -9,14 +13,25 @@ def main():
 
     # Use the tree to predict.
     test_data = pd.read_csv("datasets/decision_tree/titanic_test.csv").fillna(0)
-    test_labels = tree.predict(train_data.to_numpy()[:, :-1])
+    test_labels = tree.predict(test_data.to_numpy())
     
     # Output the predicted values to a CSV.
-    #pd.DataFrame({'PassengerId': train_data['PassengerId'],
-    #                'Survived': test_labels}).to_csv("dt_submission.csv")
+    pd.DataFrame({'PassengerId': test_data['PassengerId'],
+                    'Survived': test_labels}).to_csv("dt_submission.csv")
 
     # Output the tree to an image file.
     tree.save_tree(train_data.columns, ['No', 'Yes'])
+
+    # ----------------------------------------------------------- #
+    #                            SVM                              #
+    # ----------------------------------------------------------- #
+
+
+
+    # ----------------------------------------------------------- #
+    #                          Genetic                            #
+    # ----------------------------------------------------------- #
+
 
 
 if __name__ == "__main__":
---
+++ test/test_decision_tree.py
@@ -0,0 +1,5 @@
+import unittest
+
+def TestDecisionTree(UnitTest):
+    def test(self):
+        test = None
