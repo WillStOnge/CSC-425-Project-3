bc01a66849a2260e10b185307b26fb6206a49333
Will St. Onge <ongewill@gmail.com>
2020-11-03 17:33:43 -0500

    Completed the decision tree

---
+++ main.py
@@ -0,0 +1,23 @@
+import numpy as np, pandas as pd
+from ml_algorithms import DecisionTree
+
+def main():
+    # Train the tree.
+    train_data = pd.read_csv("datasets/decision_tree/titanic_train.csv").fillna(0)
+    tree = DecisionTree(train_data.to_numpy()[:, :-1], train_data.to_numpy()[:, -1])
+    tree.save_tree(train_data.columns, ['No', 'Yes'])
+
+    # Use the tree to predict.
+    test_data = pd.read_csv("datasets/decision_tree/titanic_test.csv").fillna(0)
+    test_labels = tree.predict(train_data.to_numpy()[:, :-1])
+    
+    # Output the predicted values to a CSV.
+    #pd.DataFrame({'PassengerId': train_data['PassengerId'],
+    #                'Survived': test_labels}).to_csv("dt_submission.csv")
+
+    # Output the tree to an image file.
+    tree.save_tree(train_data.columns, ['No', 'Yes'])
+
+
+if __name__ == "__main__":
+    main()
