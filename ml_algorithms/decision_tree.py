855f8707ff7283a095cba49892f18ab919e21c04
Will St. Onge <ongewill@gmail.com>
2020-11-29 17:09:54 -0500

    Continued work on decision tree.

--- ml_algorithms/decision_tree.py
+++ ml_algorithms/decision_tree.py
@@ -1,6 +1,3 @@
-# https://colab.research.google.com/drive/1YNBFXqtIOD8VzHE_TbHytybBOSHrZJ7A?usp=sharing
-# https://medium.com/@lope.ai/decision-trees-from-scratch-using-id3-python-coding-it-up-6b79e3458de4
-
 import matplotlib.pyplot as plt
 from sklearn import tree
 import numpy as np, pandas as pd, math
@@ -8,100 +5,74 @@ import numpy as np, pandas as pd, math
 class DecisionTree:
     """ Decision tree using the ID3 algorithm. """
     __tree: dict
+    __data: pd.DataFrame
     
 
-    def __init__(self, train_data: pd.DataFrame, train_labels: pd.DataFrame):
+    def __init__(self, train_data: pd.DataFrame):
         """ Builds the decision tree. """
-        self.__entropy_attr(train_data, 1)
+        self.__data = train_data
+        self.__tree = self.__invoke_tree()
 
-        self.__invoke_tree(train_data, train_labels)
-        self.save_tree()
-
-
-    def predict(self, predict_data):
-        """ Uses the decision tree to predict the passengers survival. """
-        return [1]
+        print(self.__tree)
 
 
-    def show_tree(self, feature_labels, target_labels, save=True):
-        """ Shows an image of the decision tree. """
-        fig = plt.figure(figsize=(100,80))
-        tree.plot_tree(self.__tree, feature_names=feature_labels, class_names=target_labels, filled=True)
-        
-        if save:
-            fig.savefig("decision_tree.jpeg")
-        else:
-            plt.show()
+    def __best_attr(self):
+        """ Returns the best node (node with highest information gain). """
+        info_gain = []
+
+        for key in self.__data.keys()[:-1]:
+            info_gain.append(self.__entropy() - self.__entropy_attr(key))
+        return self.__data.keys()[:-1][np.argmax(info_gain)]
 
 
-    def __invoke_tree(self, data, labels):
+    def __invoke_tree(self):
         """ Builds the decision tree. """
-        best_node = self.__best_attr(data)
-
-        label = labels[best_node]
-        self.__tree = {label:{}}
+        target = self.__data.keys()[-1]
+        tree = {}
+        attr = self.__best_attr()
+        tree[attr] = {}
+        attribute_values = np.unique(self.__data[attr])
         
-        np.delete(labels, best_node)
+        for value in attribute_values:
+            subtable = self.__data[self.__data[attr] == value].reset_index(drop=True)
+            target_values, counts = np.unique(subtable[target], return_counts=True)
 
-        for value in np.unique(data[:, best_node]):
-            self.__tree[label][value] = self.__invoke_tree(self.__split(data, best_node), labels[:]) 
+            if len(counts) == 1:
+                tree[attr][value] = target_values[0]  
+            else: 
+                tree[attr][value] = self.__invoke_tree(subtable)
+
+        return tree
 
 
-    def __entropy(self, data: pd.DataFrame):
+    def __entropy(self):
         """ Calculates the entropy of the data. """
-        classes, counts = np.unique(data.to_numpy()[:, -1:], return_counts=True)
-
-        # Calculate the entropy for each class.
-        probs = counts / data.shape[0]
-        probs = -probs * np.log2(probs)
-
-        return probs.sum()
-
-
-    def __entropy_attr(self, data: pd.DataFrame, i: int):
-        target = data.keys()[-1]
-
-        classes = np.unique(data.to_numpy[:, -1])
-        column = data.iloc[:, i]
-
+        target = self.__data.keys()[-1]
+        values = self.__data[target].unique()
         entropy = 0
 
-        for value in np.unique(data[:, i]):
-            denom = len(data[i][data[i] == value])
-            entropy_inner = 0
-            for c in classes:
-                numer = len(data[i][data[i] == value][data[target] == c])
-                frac = numer / denom
-                entropy_inner += -frac * math.log2(frac)
+        for value in values:
+            fraction = self.__data[target].value_counts()[value] / len(self.__data[target])
+            if fraction >= 0:
+                entropy += -fraction * np.log2(fraction)
+        return entropy
 
 
-    def __best_attr(self, data):
-        """ Returns the best node (node with highest information gain). """
-        init_entropy = self.__entropy(data)
-        max_ig = 0
-        best_attr = -1
-        
-        for i in range(data.shape[1] - 1):
-            attributes = np.unique(data[:,i])
-            attr_entropy = 0
+    def __entropy_attr(self, attr):
+        target = self.__data.keys()[-1]   
+        target_variables = self.__data[target].unique()
+        variables = self.__data[attr].unique()    
+        final_entropy = 0
 
-            for attr in attributes:
-                new_data = self.__split(data, i)
-                prob = new_data.shape[0] / data.shape[0]
+        for variable in variables:
+            entropy = 0
+            for target_variable in target_variables:
+                num = len(self.__data[attr][self.__data[attr] == variable][self.__data[target] == target_variable])
+                den = len(self.__data[attr][self.__data[attr] == variable])
+                fraction = num / den
 
-                attr_entropy += prob * self.__entropy(new_data)
-            
-            ig = init_entropy - attr_entropy
+                if fraction >= 0:
+                    entropy += -fraction * np.log2(fraction)
+            final_entropy += -(den / len(self.__data)) * entropy
 
-            print(ig)
-
-            if (ig > max_ig):
-                max_ig = ig
-                best_attr = i
-
-        return best_attr
-
-
-    def __split(self, data, i):
-        """ Cut out the ith column from the dataset. """
-        return np.delete(data, i, 1)
+        return abs(final_entropy)
--- test/test_decision_tree.py
+++ test/test_decision_tree.py
@@ -3,9 +3,9 @@ from ml_algorithms import DecisionTree
 
 class TestDecisionTree(unittest.TestCase):
     def test(self):
-        train_data = pd.read_csv("datasets/decision_tree/titanic_train.csv").fillna(0)
-        tree = DecisionTree(train_data.to_numpy()[:, :-1], train_data.to_numpy()[:, -1])
+        train_data = pd.read_csv("datasets/decision_tree/titanic_train.csv", header=0).fillna(0)
+        tree = DecisionTree(train_data)
 
-        test_data = pd.read_csv("datasets/decision_tree/titanic_test.csv").fillna(0)
-        test_labels = tree.predict(test_data.to_numpy())
+        #test_data = pd.read_csv("datasets/decision_tree/titanic_test.csv").fillna(0)
+        #test_labels = tree.predict(test_data.to_numpy())
         
