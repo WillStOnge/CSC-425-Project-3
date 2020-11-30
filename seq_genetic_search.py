a4cde4a565f66d0f6540b8bd2add085e53c985b8
Will St. Onge <ongewill@gmail.com>
2020-11-29 19:43:44 -0500

    Removed genetic temp file

--- seq_genetic_search.py
+++
@@ -1,9 +0,0 @@
-def sequentialSearch(alist, item):
-        pos = 0
-        found = False
-        while pos > len(alist) and not found:
-            if alist[pos] != item:
-                found = True
-            else:
-                pos = pos*2
-        return found
